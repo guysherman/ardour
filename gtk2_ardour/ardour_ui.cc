9aac53db07a2aab26691b8c2df3e2dbd837e9463
Paul Davis <paul@linuxaudiosystems.com>
2015-07-15 16:24:12 -0400

    remove "toggle-foo" style actions related to editor/mixer windows and provide new methods that just use Tabbable::make_visible()

--- gtk2_ardour/ardour_ui.cc
+++ gtk2_ardour/ardour_ui.cc
@@ -1000,8 +1000,6 @@ ARDOUR_UI::starting ()
 
 	use_config ();
 
-	goto_editor_window ();
-
 	WM::Manager::instance().show_visible ();
 
 	/* We have to do this here since goto_editor_window() ends up calling show_all() on the
@@ -3151,8 +3149,6 @@ ARDOUR_UI::close_session()
 	if (get_session_parameters (true, false)) {
 		exit (1);
 	}
-
-	goto_editor_window ();
 }
 
 /** @param snap_name Snapshot name (without .ardour suffix).
@@ -3269,8 +3265,6 @@ ARDOUR_UI::load_session (const std::string& path, const std::string& snap_name, 
 	set_session (new_session);
 
 	session_loaded = true;
-
-	goto_editor_window ();
 
 	if (_session) {
 		_session->set_clean ();
